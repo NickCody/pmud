19d7ec4a29a6b902275937fddb6872be892c460f
Nick Codignotto <nick.codignotto@gmail.com>
2021-11-18 00:57:18 +0000

    cli options + new binaries

--- graph/fully-connected.cpp
+++ graph/fully-connected.cpp
@@ -6,27 +6,6 @@
 
 #include "graph.h"
 
-void addEdges1(Graph& graph) {
-    graph.addDirectedEdge(0, 1);
-    graph.addDirectedEdge(0, 4);
-    graph.addDirectedEdge(1, 2);
-    graph.addDirectedEdge(1, 3);
-    graph.addDirectedEdge(1, 4);
-    graph.addDirectedEdge(2, 3);
-    graph.addDirectedEdge(3, 4);
-}
-
-void addEdges2(Graph& graph) {
-    graph.addEdge(0, 1);
-}
-
-void addEdges3(Graph& graph) {
-    graph.addEdge(0, 1);
-    graph.addEdge(0, 2);
-    graph.addEdge(1, 2);
-    //graph.addEdge(2, 0);
-}
-
 void addEdgesRandom(Graph& graph, int N) {
     for (int i=0; i < N; i++) {
         graph.addEdge(rand() % graph.numVertices(), rand() % graph.numVertices());
@@ -48,7 +27,12 @@ int main(int argc, char** argv)
     std::setlocale(LC_ALL, "en_US.UTF-8");
     srand(time(0));
 
-    Graph graph(20, false);
+    int V=5;
+
+    if (argc == 2)
+        V = atoi(argv[1]);
+
+    Graph graph(V, false);
     addEdgesAllConnect(graph);
     graph.printGraph(true);
 
