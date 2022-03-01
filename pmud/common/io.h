e6567676551828f799eaa9c8ab256515c400fd65
nick.codignotto@gmail.com <Nick Codignotto>
2022-03-01 13:38:35 +0000

    Started yaml dump to redis...

---
+++ pmud/common/io.h
@@ -0,0 +1,38 @@
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstring>
+
+#include "fmt/format.h"
+
+namespace primordia::mud::common {
+
+  using namespace std;
+
+  struct StreamDelete {
+    void operator()(istream* is_ptr) const {
+      if (is_ptr && is_ptr != &cin) {
+        static_cast<ifstream*>(is_ptr)->close();
+        delete is_ptr;
+      }
+    }
+  };
+
+  inline unique_ptr<istream, StreamDelete> stdout_or_file(const char* filename) {
+
+    StreamDelete deletor;
+    unique_ptr<istream, StreamDelete> is_ptr{ nullptr, deletor };
+
+    if (filename == nullptr || strlen(filename) == 0) {
+      fmt::print(stderr, "Reading YAML from stdin...\n");
+      is_ptr.reset(&cin);
+    } else {
+      is_ptr.reset(new ifstream(filename));
+    }
+
+    return is_ptr;
+  }
+
+} // namespace primordia::mud::common
--- util/yaml-redis-loader.cpp
+++ util/yaml-redis-loader.cpp
@@ -1,17 +1,39 @@
+#include "common/io.h"
+
 #include "spdlog/spdlog.h"
+#include "yaml-cpp/yaml.h"
 
 #include "storage/storage.h"
 
+using namespace std;
 using namespace primordia::mud::storage;
+using namespace primordia::mud::common;
 
-int main(int /*argc*/, char** /*argv*/) {
+int main(int argc, char** argv) {
 
   auto storage = initialize_redis_storage();
 
-  if (storage && !storage->init()) {
+  if (!storage) {
     SPDLOG_ERROR("Failed to initialize redis storage.");
     exit(1);
   }
 
+  string filename;
+  int c;
+  opterr = 0;
+  while ((c = getopt(argc, argv, "f:")) != -1) {
+    switch (c) {
+    case 'f':
+      filename = optarg;
+      break;
+    default:
+      exit(1);
+    }
+  }
+
+  auto input = stdout_or_file(filename.c_str());
+
+  YAML::Node config = YAML::Load(*input);
+  fmt::print("{}\n", config["server"]["name"].as<string>());
   return 0;
 }
