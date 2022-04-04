70ae9105d27b6a6c77caf193d4c14b3b05432fa3
nick.codignotto@gmail.com <Nick Codignotto>
2022-04-04 13:05:29 +0000

    working actor unit tests!

--- pmud/test/actor/quick_test.cpp
+++ pmud/test/actor/quick_test.cpp
@@ -1,22 +1,28 @@
+#include <caf/fwd.hpp>
 #include <memory>
 #include <iostream>
 #include <caf/all.hpp>
+#define CAF_TEST_NO_MAIN
 #include <caf/test/unit_test_impl.hpp>
 #include <caf/test/dsl.hpp>
+#include <fmt/format.h>
+
+using namespace std;
+using namespace caf;
 
 CAF_BEGIN_TYPE_ID_BLOCK(demo_types, caf::first_custom_type_id)
 CAF_ADD_ATOM(demo_types, pmud, Noop)
 CAF_END_TYPE_ID_BLOCK(demo_types)
 
-using namespace std;
-using namespace caf;
-
-using Message = pmud::Noop;
-
-// using Message = caf::get_atom;
+int main(int argc, char** argv) {
+  using namespace caf;
+  init_global_meta_objects<id_block::demo_types>();
+  core::init_global_meta_objects();
+  return test::main(argc, argv);
+}
 
 behavior TestActor(event_based_actor*) {
-  return { [=](Message) {} };
+  return { [=](pmud::Noop) {} };
 }
 
 struct basic_demo_fixture : test_coordinator_fixture<> {
@@ -26,17 +32,20 @@ struct basic_demo_fixture : test_coordinator_fixture<> {
     my_actor = sys.spawn(TestActor);
     run();
   }
+
+  ~basic_demo_fixture() {
+    anon_send_exit(my_actor, exit_reason::user_shutdown);
+  }
 };
 
 CAF_TEST_FIXTURE_SCOPE(demo_tests, basic_demo_fixture)
 
 CAF_TEST(quick test) {
-  self->send(my_actor, Message());
-  sched.run_once();
+  self->send(my_actor, pmud::Noop());
 
-  expect((Message), from(self).to(my_actor).with(_));
+  expect((pmud::Noop), from(self).to(my_actor));
 
   //  No further messages allowed.
-  disallow((Message), from(self).to(my_actor).with(_));
+  disallow((pmud::Noop), from(self).to(my_actor));
 }
 CAF_TEST_FIXTURE_SCOPE_END()
